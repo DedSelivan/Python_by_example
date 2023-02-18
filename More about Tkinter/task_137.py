b7a5bd05776dee2efceaf2fc97d3606c1c7f3cf7
DedSelivan <dedselivan@192.168.1.8>
2023-02-18 23:23:57 +0300

    Добавлено решение задач по 138

--- More about Tkinter/task_137.py
+++ More about Tkinter/task_137.py
@@ -3,3 +3,48 @@
 # Измените программу 136, чтобы новые значения имени и пола, добавляемые в список,
 # так же записывались в текстовый файл. 
 # Добавьте еще одну кнопку, которая будет выводить весь текстовый файл в главном окне командной оболочки Python.
+from tkinter import *
+
+def add_to_list():
+    name = nameBox.get()
+    nameBox.delete(0, END)
+    checkGender = gender.get()
+    gender.set('М/Ж')
+    newList = f'{name}, {checkGender} \n'
+    myList.insert(END, newList)
+    nameBox.focus()
+    myFile = open('MyNameList.txt', 'a')
+    myFile.write(newList)
+
+def readList():
+    myFile = open('MyNameList.txt', 'r')
+    print(myFile.read())
+
+app = Tk()
+app.title('Список людей')
+app.geometry('400x440')
+
+nameText = Label(text='Введите имя:')
+nameText.place(x=20, y=20, width=100, height=30)
+
+nameBox = Entry(text='')
+nameBox.place(x=120, y=20, width=150, height=30)
+nameBox.focus()
+
+gendText = Label(text='Выберите пол')
+gendText.place(x=20, y=80)
+gender = StringVar(app)
+gender.set('М/Ж')
+gendermenu = OptionMenu(app, gender, 'М', 'Ж')
+gendermenu.place(x=120, y=80)
+
+myList = Listbox()
+myList.place(x=20, y=120, width=300, height=100)
+
+addButton = Button(text='Добавить', command=add_to_list)
+addButton.place(x=220, y=80, width=100, height=25)
+
+add_csvButton = Button(text='PRINT', command=readList)
+add_csvButton.place(x=220, y=250, width=100, height=25)
+
+app.mainloop()
--- More about Tkinter/task_138.py
+++ More about Tkinter/task_138.py
@@ -5,3 +5,36 @@
 # Убедитесь в том, что все файлы хранятся в формате GIF. 
 # Выведите один из них в окне и предложите пользователю ввести число. 
 # Введенное число используется для выбора правильного имени файла и вывода изображения.
+from tkinter import *
+
+def click():
+    number = imgBox.get()
+    numbIMG = f'More about Tkinter/images/{number}.gif'
+    myImage = PhotoImage(file=numbIMG)
+    myImageBox.image = myImage
+    myImageBox['image'] = myImage
+    myImageBox.update()
+    imgBox.delete(0, END)
+    imgBox.focus()
+
+app = Tk()
+app.title('Вывод изображений')
+app.geometry('400x350')
+
+myImage = PhotoImage(file='More about Tkinter/images/1.gif')
+myImageBox = Label(app, image=myImage)
+myImageBox.image = myImage
+myImageBox.place(x=100, y=20, width=256, height=256)
+
+myText = Label(text='Выберите номер изображения:')
+myText.place(x = 20, y = 300, width = 200, height = 25)
+
+imgBox = Entry(text='')
+imgBox.place(x = 220, y = 300, width = 50, height = 25)
+imgBox.focus()
+
+imgButton = Button(text='Показать', command=click)
+imgButton.place(x = 280, y = 300, width = 100, height = 25)
+
+
+app.mainloop()
